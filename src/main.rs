c009b126135daf6a3f385915b826c5ae1dd35576
Vasya Novikov <n1dr+cm3513git@ya.ru>
2018-04-23 22:24:25 +0300

    remove --ok-all-env key

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,10 @@ use std::process::Command;
 use std::env;
 use std::collections::HashSet;
 use std::ops::Deref;
-use std::ffi::OsString;
 use std::os::unix::process::CommandExt;
 use std::borrow::BorrowMut;
 
-fn restore_env(cmd: &mut Command, env_var: &str) {
-	if let Some(old) = env::var_os(OsString::from(env_var)) {
-		cmd.env(env_var, old);
-	}
-}
-
 fn main() {
-	// TODO:
-	// --ok-env (preserve an env variable)
-	// --bind-eq (bind to same path)
 	let exclude_args: HashSet<_> = [
 		"--ok-net",
 		"--ok-ipc",
@@ -27,7 +17,6 @@ fn main() {
 		"--ok-cgroup",
 		"--ok-parent",
 		"--ok-session",
-		"--ok-all-env",
 	].iter().cloned().collect();
 	let cleaned_args: Vec<_> = env::args().skip(1).filter(
 		|x| !exclude_args.contains(x.deref())
@@ -63,10 +52,6 @@ fn main() {
 		command.arg("--new-session");
 	}
 	command.args(cleaned_args);
-
-	if !arg_set.contains("--ok-all-env") {
-		command.env_clear();
-	}
 
 	let fork_error: std::io::Error = command.exec();
 	eprintln!("ERROR running bwrap: {}", fork_error.to_string());
