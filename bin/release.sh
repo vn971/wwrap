579d0b7a342f50322c64954956feb36c73c5be73
Vasili Novikov <n1dr+cm3513git@ya.ru>
2020-08-15 09:06:36 +0200

    Update release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,7 +1,21 @@
 #!/bin/bash -euET
 {
-cargo build --release
-cp -a target/release/wwrap ~/bin/wwrap
+
+rustup update
+
+cargo upgrade
+cargo update
+
+cargo fmt --all --
+cargo clippy --all-targets --all-features -- -D warnings
+
+if ! test -z "$(git status --porcelain)"; then
+  >&2 printf '%s\n' "error: uncommitted changes"
+  exit 1
+fi
+
+cargo build --release --target=x86_64-unknown-linux-musl
+cp -a target/x86_64-unknown-linux-musl/release/wwrap ~/bin/wwrap
 
 exit
 }
